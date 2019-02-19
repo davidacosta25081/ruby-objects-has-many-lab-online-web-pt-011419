aadc8e9f033af83f1e144061dec3d3a64a1ca694
David Acosta <davidacosta25081@yahoo.com>
2019-02-19 23:58:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,13 +2,17 @@ class Artist
 
 attr_accessor :name
 
-  @@songs = []
+  @@all = []
 
    def initialize(name)
     @name = name
-    @@songs << self
+    @@all << self
 
   end
+
+def self.all 
+  @all
+end
 
 def add_song (song)
   @songs << song
